3937a701e26572a106106d2ff5e431e95abd98fd
danichol <daneelnichol@outlook.com>
2021-03-24 21:52:06 -0500

    Updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .config import Config
 from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+from flask_SQLAlchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
