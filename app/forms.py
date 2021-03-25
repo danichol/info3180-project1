93163db7cd5cfa7ef4d75fb431ccd6e0bf238061
danichol <daneelnichol@outlook.com>
2021-03-24 21:05:03 -0500

    Add new property form

---
+++ app/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms.fields import StringFIeld, PasswordField, IntegerField, TextField, FloatField, FileField, TextAreaField, SelectField
+from wtforms.validators import DataRequired
+from flask.helpers import send_from_directory
+
+class PropertyForm(FlaskForm):
+    title = TextField("Property Title", validators =[DataRequired()])
+    description = TextAreaField("Description", validators =[DataRequired()])
+    bedroom = TextField("No. of Rooms", validators =[DataRequired()])
+    bathroom = TextField("No. of Bathrooms", validators =[DataRequired()])
+    price = TextField("Price", validators=[DataRequired()])
+    ptype= SelectField("Property Type", choices=[('House','House'),('Apartment','Apartment')] ,validators = [DataRequired()])
+    location = TextField("Location", validators = [DataRequired()]) 
+    photo = ("Photo", validators = [FileAllowed(['jpg','png'],"Please use only jpg or png images!"), FileRequired()])
+
